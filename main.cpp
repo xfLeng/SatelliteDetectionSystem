eee1f80141b3d27b23bdd7ee4274194debb465a9
The_Emperor1 <928683372@qq.com>
2017-08-18 16:17:29 +0800

    时间：2017-8-18 16：17 提交人：冷小凡 详情：测试

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    // QFont font("Î¢ÈíÑÅºÚ", 9);
-    // app.setFont(font);
-
     MainWidget main_widget;
     main_widget.showWidget();
 
