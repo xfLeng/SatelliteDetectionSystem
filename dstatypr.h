854d9d4d5c1d57ecc3f2eecbf36a48cb2b936d28
The_Emperor1 <928683372@qq.com>
2017-08-21 14:50:34 +0800

    时间：2017-8-21 14：50 提交人：冷小凡 详情：无

---
+++ dstatypr.h
@@ -0,0 +1,11 @@
+#ifndef DATATYPR_H
+#define DATATYPR_H
+typedef struct MFaultParametervalue
+{
+	int  startime;//开始时间
+	int type;//1阶跃 2斜坡
+	int lenth;//时长
+	int SatNO;//卫星号
+	double value;//变化值
+}FaultParametervalue;
+#endif // DATATYPR_H
